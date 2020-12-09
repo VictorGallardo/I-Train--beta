6b76d7bd63d44e5f290f1783f5748bf5daf28292
VictorGallardo <victor.gallardo@iescampanillas.com>
2020-12-09 15:37:44 +0100

    Actualizado

--- i-train/src/app/home/home.page.ts
+++ i-train/src/app/home/home.page.ts
@@ -12,16 +12,25 @@ import { AlertController } from '@ionic/angular';
 export class HomePage {
 
   btnColor = "bluelight";
+  toggle: boolean = false;
 
   constructor(public exercisesService: ExerciseService,
     private router: Router) { }
 
   changeColor(btn) {
-
     if (btn.color === 'bluelight')
       btn.color = 'yellow';
     else
       btn.color = 'bluelight';
+  }
+  reorderItem(event) {
+    if (this.toggle == false) {
+      this.toggle = true;
+
+    } else {
+      this.toggle = false
+      event.detail.complete();
+    }
   }
 
   goAddEdit(id: number) {
@@ -31,6 +40,7 @@ export class HomePage {
   goTimer(id: number) {
     this.router.navigateByUrl(`/timer${id != undefined ? '/' + id : ''}`);
   }
+
 
 
   ///////////////////////////////////////////////////////////////////
